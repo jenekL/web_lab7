bb51980668b32249126d634f37340b222c6f79e8
Jenya <39410400+jenekL@users.noreply.github.com>
2019-10-13 11:40:07 +0300

    Update Comment.vue

--- src/components/Comment.vue
+++ src/components/Comment.vue
@@ -1,5 +1,7 @@
 <template>
-   <p>{{comment}}</p>
+   <p>{{comment.user}}</p>
+    <p>{{comment.text}}</p>
+     <p>{{comment.date}}</p>
 </template>
 
 <script>
@@ -12,18 +14,5 @@
 </script>
 
 <style scoped>
-    h3 {
-        margin: 40px 0 0;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-    }
-    li {
-        display: inline-block;
-        margin: 0 10px;
-    }
-    a {
-        color: #42b983;
-    }
+  
 </style>
