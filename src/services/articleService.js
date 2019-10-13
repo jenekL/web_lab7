6b4b388a21aff0f4ec641ff6552cf8e765b92faa
Lomakin Yevhen <jeklom@gmail.com>
2019-10-13 12:00:35 +0300

    add article and comment

--- src/services/articleService.js
+++ src/services/articleService.js
@@ -6,4 +6,10 @@ export default {
 
     return response.data;
   },
+  async addArticle () {
+    const response = await api().post('/article/add');
+  },
+  async addComment (postId) {
+    const response = await api().post(`/comment/add?articleId=${postId}`);
+  },
 }
